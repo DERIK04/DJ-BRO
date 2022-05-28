a1dac1a9933e7d3b67e2bf038368ecc48d91130f
DERIK04 <106422583+DERIK04@users.noreply.github.com>
2022-05-28 11:59:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function song1() {
+    document.getElementById("1").play();
+  }
+  function song2() {
+    document.getElementById("2").play();
+  }
+  function song3() { 
+    document.getElementById("3").play();
+  }
+   function stop1() {
+    document.getElementById("1").pause();
+    document.getElementById("2").pause();
+    document.getElementById("3").pause();
+   }
+  
